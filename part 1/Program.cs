69347e59cbac9cbe496e38fb1b962e9e9b31bf59
hyojin park <candy020406@naver.com>
2022-09-12 12:58:22 +0900

    초기 커밋.

---
+++ part 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class PrimitiveTypeApp
+{
+    public static void Main()
+    {
+        bool b;
+        int i = int.MaxValue;//모든 자료형이c#에서 객체로 처리됨
+        uint j = uint.MaxValue;
+        double d = double.MaxValue;
+        b = (i != 0);
+        Console.WriteLine("boolean b = " + b);
+        Console.WriteLine("max value of signed integer = " + i);
+        Console.WriteLine("max value of unsigned integer = " + j);
+        Console.WriteLine("max value of double = " + d);
+    }
+}
